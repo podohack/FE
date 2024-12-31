a10b1bb4a3fb125609fe504696715512daf415e9
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-12-31 15:38:42 +0900

    ✨feat: 알림 스크린 페이지 구현

---
+++ src/custom.d.ts
@@ -0,0 +1,22 @@
+//이미지 파일에 대한 타입 선언
+
+declare module '*.png' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.jpg' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.jpeg' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.svg' {
+    const content: any;
+    export default content;
+  }
+  
